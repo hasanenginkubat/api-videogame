d57fb44204c5f4e333b9e784ce70e5d5a81e8c27
hasanenginkubat <kubathasanengin@gmail.com>
2023-10-04 16:51:49 -0500

    render

--- src/db.js
+++ src/db.js
@@ -2,41 +2,20 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const DB_USER = "postgres"
-const DB_HOST = "localhost"
-const DB_NAME = "video"
-const DB_PORT = 5432
-const DB_PASSWORD = "dalakDalak99"
 
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: DB_PORT,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-        { logging: false, native: false }
-      );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: false,
+  },
+  pool: {
+    max: 3,
+    min: 1,
+    idle: 10000,
+  },
+});
 
 //aca nos conectamos a la base de datos videogames
 // es la conex que hacemos para que sequelize use nuestra base de datos
