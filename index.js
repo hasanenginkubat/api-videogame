bc3d2a7885f256639ddbe66213e045b7e5889215
hasanenginkubat <kubathasanengin@gmail.com>
2023-10-09 10:20:20 -0500

    database

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+require('dotenv').config();
+const { Pool } = require('pg');
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { sslmode: 'require' },
 });
+
+pool.connect()
+  .then(() => {
+    console.log('Connected to the database');
+    return pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
+    `);
+  })
+  .then(() => {
+    return pool.query(`
+      CREATE TABLE IF NOT EXISTS videogame (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        name VARCHAR(255) NOT NULL UNIQUE,
+        description TEXT NOT NULL,
+        released DATE,
+        rating FLOAT CHECK (rating >= 1.0 AND rating <= 5.0),
+        platforms TEXT[] NOT NULL,
+        background_image TEXT NOT NULL,
+        createdInDb BOOLEAN NOT NULL DEFAULT true
+      );
+    `);
+  })
+  .then(() => {
+    console.log('Tables created successfully');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = pool;
